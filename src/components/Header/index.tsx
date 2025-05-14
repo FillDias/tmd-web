8d21052084488292101fadb964e06a5ded239f04
Felipe <dias.felipecarlos@gmail.com>
2025-05-14 16:22:31 -0300

    header tmd web

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+export const Header = ()=>{
+    return(
+        <>
+<div className=" z-10 top-0 left-0 right-0 absolute flex items-center justify-center mt-6">
+<section className=" w-[315px] h-[28px]  flex justify-between items-center  rounded-[20px] bg-[rgba(83,83,83,0.2)]">
+
+</section>
+
+
+</div>
+        
+        </>
+    )
+}
